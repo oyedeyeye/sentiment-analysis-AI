a53b29845a1e4ff5aa26f3ae4adb9b20f08a0170
Hezekiah Oyemade <oyedey@gmail.com>
2024-02-08 17:35:04 +0100

    code refactoring and linting

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -1,4 +1,8 @@
-import requests, json
+"""
+sentiment_analyzer: leverage IBM Watson AI library to conduct sentiment analysis on input text
+"""
+import json
+import requests
 
 # text_to_analyse = "I love this new technology."
 
@@ -8,14 +12,23 @@ def sentiment_analyzer(text_to_analyse):
     Arguments:
         text_to_analyse: string dict that is to be analyzed
     """
-    url = 'https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
+    url = 'https://sn-watson-sentiment-bert.labs.skills.network/\
+            v1/watson.runtime.nlp.v1/NlpService/SentimentPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
 
-    response = requests.post(url, json = myobj, headers=header)
+    response = requests.post(url, json = myobj, headers=header, timeout=60)
     # Convert response text to json/dict
     formatted_response = json.loads(response.text)
+    if response.status_code == 200:
+        label = formatted_response['documentSentiment']['label']
+        score = formatted_response['documentSentiment']['score']
+
+    elif response.status_code == 500:
+        label = None
+        score = None
+
     return {
-        'label': formatted_response['documentSentiment']['label'],
-        'score': formatted_response['documentSentiment']['score']
+        'label': label,
+        'score': score
     }
--- server.py
+++ server.py
@@ -3,10 +3,10 @@
     localhost:5000.
 '''
 # Import Flask, render_template, request from the flask pramework package : TODO
-from flask import Flask, render_template, requests
+from flask import Flask, render_template, request
 
 # Import the sentiment_analyzer function from the package created: TODO
-from SentimentAnalysis.sentiment_analysis import sent_analyzer
+from SentimentAnalysis.sentiment_analysis import sentiment_analyzer
 
 #Initiate the flask app : TODO
 app = Flask(__name__)
@@ -19,23 +19,25 @@ def sent_analyzer():
         function. The output returned shows the label and its confidence 
         score for the provided text.
     '''
-    # TODO
+    # Get text imput from request argument
     text_to_analyze = request.args.get('textToAnalyze')
+
     # Run Sentiment analysis of the input text
     result = sentiment_analyzer(text_to_analyze)
 
-    return f"The given text has been identified as {result['label']} with a score of {result['score']}."
+    if result['label'] is None:
+        return "Invalid input ! Try again."
+
+    return f"The given text has been identified as {result['label'].split('_')[1]} \
+                with a score of {result['score']}."
 
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
         page over the Flask channel
     '''
-    #TODO
-    render_template('index.html')
+    return render_template('index.html')
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
-    app.run(host="0.0.0.0", port=5000)
     app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
